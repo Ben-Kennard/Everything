1a1ed3a02a7032557c124786ff141b550bdbfea2
Ben-Kennard <32109266+Ben-Kennard@users.noreply.github.com>
2017-11-10 14:24:23 -0500

    Update Staff.java

--- University/src/university/Staff.java
+++ University/src/university/Staff.java
@@ -14,15 +14,32 @@ public class Staff extends UEmployee{
     private String jobTitle;
     private String employeeName;
     private double employeeSalary;
+   /**
+   Constructor
+   pre: none
+   post: a staff obect is created 
+   */
+    
     Staff(String tempJobTitle,String employeeName1,int employeeSalary1){
         super(employeeName1,employeeSalary1);
         jobTitle = tempJobTitle;
         employeeName = employeeName1;
         employeeSalary = employeeSalary1;
     }
+    /**
+    Returns the job title 
+    pre: none 
+    post: the job title
+    */
     public String getJobTitle(){
         return(jobTitle);
     }
+    
+    /**
+    Returns a string that represents the employee name, salary and job title
+    pre: none 
+    post: a string representing the employee name, salary and job title is returned 
+    */
 public String toString(){
     return("The employees name is: " + employeeName + "\n" + "The salary is "+ employeeSalary+
             "\n"+"and their title is: " + jobTitle);
